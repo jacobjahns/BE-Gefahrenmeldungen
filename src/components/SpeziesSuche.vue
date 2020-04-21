acced868567c50ba6ca7d4499aac7f1f36a3760a
Jacob <glue.crafter@gmail.com>
2020-04-21 12:39:58 +0200

    version 1.0

--- src/components/SpeziesSuche.vue
+++ src/components/SpeziesSuche.vue
@@ -20,7 +20,7 @@ export default {
     return {
       lastRequest: 0,
       searchInput: "",
-      species: [{"id":4228,"name":"Aalmutter","scienceName":"Zoarces viviparus","slug":"zoarces-viviparus"},{"id":6335,"name":"Ei Trottellumme","scienceName":"Uria aalge (ovum)","slug":"uria-aalge-ovum"},{"id":6294,"name":"Fl\u00fcgel Trottellumme","scienceName":"Uria aalge (ala)","slug":"uria-aalge-ala"},{"id":6295,"name":"Fl\u00fcgel Trottellumme Jungvogel","scienceName":"Uria aalge (ala iuvenilis)","slug":"uria-aalge-ala-iuvenilis"},{"id":4202,"name":"Flussaal","scienceName":"Anguilla anguilla","slug":"anguilla-anguilla"},{"id":5504,"name":"Glasaal","scienceName":"Anguilla anguilla (juvenile)","slug":"anguilla-anguilla-juvenile"},{"id":4206,"name":"Gro\u00dfer Sandaal","scienceName":"Hyperoplus lanceolatus","slug":"hyperoplus-lanceolatus"},{"id":4208,"name":"Kleiner Sandaal","scienceName":"Ammodytes tobianus","slug":"ammodytes-tobianus"},{"id":4203,"name":"Meeraal","scienceName":"Conger conger","slug":"conger-conger"},{"id":4210,"name":"Nacktsandaal","scienceName":"Gymnammodytes semisquamatus","slug":"gymnammodytes-semisquamatus"},{"id":6500,"name":"Sch\u00e4del Trottellumme","scienceName":"Uria aalge (cranium)","slug":"uria-aalge-cranium"},{"id":5450,"name":"Trottellumme","scienceName":"Uria aalge","slug":"uria-aalge"},{"id":4207,"name":"Ungefleckter Sandaal","scienceName":"Hyperoplus immaculatus","slug":"hyperoplus-immaculatus"}]
+      species: []
     }
   },
   methods: {
