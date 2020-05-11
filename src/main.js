1954bf2c955b7cc5b2a3ce6aac5037c8523f5e24
Jacob <glue.crafter@gmail.com>
2020-05-11 17:02:57 +0200

    cors testing

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 Vue.mixin({
+  data() {
+    return {
+      header: new Headers({'Authorization': 'Basic ' + btoa('beachex:beachex')})
+    }
+  },
   methods: {
     getQueryParams() {
       let q = this.$route.query.species;
