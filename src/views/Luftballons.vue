6eeca071c799fe029995755ca888dfa38eee6039
J4c0b <glue.crafter@gmail.com>
2020-05-19 17:03:00 +0200

    style changes & router mode reset

--- src/views/Luftballons.vue
+++ src/views/Luftballons.vue
@@ -1,8 +1,10 @@
 <template>
+<div class="preview-page"><!-- delete for production -->
+<div class="preview-container"><!-- delete for production -->
   <main class="luftballons">
     <section class="header">
       <h2 class="headline">Luftballons</h2>
-      <p class="last-update">vor <span>{{ Math.floor(Date.now() - this.entries[0].findingDate / 86400000) }}</span> Tagen</p>
+      <p class="last-entry">zuletzt vor <span>{{ Math.floor( getTimePassed(new Date(this.entries[0].findingDate)) / 86400000) }}</span> Tagen</p>
     </section>
     <section class="entries">
       <div class="entry" v-for="entry in entries" :key="entry.id">
@@ -11,6 +13,8 @@
       </div>
     </section>
   </main>
+</div><!-- delete for production -->
+</div><!-- delete for production -->
 </template>
 
 <script>
@@ -19,7 +23,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      entries: [],
+      entries: [{findingDate:0}],
       lastUpdate: 0,
       apiRequest: 'https://api.stage.beachexplorer.org/v2/determination/forSpecies/ospar-kategorie-49?_format=json&order=DESC',
       limitEntries: 20,
@@ -27,12 +31,14 @@ export default {
     }
   },
   methods: {
-    getTimePassed() {
+    getTimePassed(timestamp) {
       // returns time since last update in ms
       
-      let t = Date.now() - this.entries[0].findingDate;
-      t = Math.floor(t / 86400000);
-      return t;
+      try {
+        return Date.now() - timestamp;
+      } catch(err) {
+        return 0;
+      }
     },
     getEntries() {
       // (re)loads the entries in the page
@@ -54,10 +60,15 @@ export default {
 @import '../style/_mixins.scss';
 
 
+// main app styling
 .luftballons {
   position: absolute;
   width: 100%; // change to simulate other environments
   height: 100%;
+
+  padding: 2px 4px;
+  background-color: var(--color-background);
+  overflow: hidden;
 
   display: flex;
   flex-direction: column;
@@ -65,6 +76,10 @@ export default {
 }
 
 .header {
+  margin: 2px 0;
+  padding: 0 0 2px;
+  border-bottom: 1px solid var(--color-border);
+
   display: flex;
   flex-direction: row;
   align-items: baseline;
@@ -88,13 +103,36 @@ export default {
   flex-direction: column;
   align-items: flex-start;
 
+  overflow: auto;
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+  &::-webkit-scrollbar-track {
+    background: var(--color-background);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: var(--color-border);
+    border-radius: 30px;
+    border: none;
+  }
+  scrollbar-width: thin;
+  scrollbar-color: var(--color-border) var(--color-background);
+
   .entry {
     width: 100%;
     margin: 2px 0;
     padding: 4px 4px;
+
     border-collapse: collapse;
     border-top: 1px dotted var(--color-border);
     border-bottom: 1px dotted var(--color-border);
+    transition: all .2s;
+
+    &:hover {
+      border-top: 1px solid var(--color-border);
+      border-bottom: 1px solid var(--color-border);
+      background-color: darken($color-background, 5%);
+    }
 
     display: flex;
     flex-direction: row;
@@ -104,6 +142,23 @@ export default {
     .date {
       font-weight: normal;
     }
+  }
+}
+
+
+// preview styling
+.preview-page {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+
+  .preview-container {
+    position: absolute;
+    width: 360px;
+    height: 60%;
+    top: 20px;
+    right: 20px;
   }
 }
 </style>
